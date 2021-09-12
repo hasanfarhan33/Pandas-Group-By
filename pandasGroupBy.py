9ac85024137a5f367c1052f8c4e54aa935b7256a
Farhan Hasan <hasanfarhan33@gmail.com>
2021-09-12 09:26:24 +0300

    Initial Commit

---
+++ pandasGroupBy.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+data = {'Company':['GOOG','GOOG','MSFT','MSFT','FB','FB'],
+       'Person':['Sam','Charlie','Amy','Vanessa','Carl','Sarah'],
+       'Sales':[200,120,340,124,243,350]}
+
+df = pd.DataFrame(data)
+print(df)
